36771cbea72bdf3a39e59cf358d3e00f0f32a9f3
dobbbo <dobson.adam21@gmail.com>
2022-09-19 23:13:51 +1000

    Created README.md file and wrote out a general flow of operations for my 2048 game.

---
+++ main.py
@@ -0,0 +1,7 @@
+# Things to do:
+# 1. Set up board using a list of lists. Create a temporary board.
+# 2. Create functions that will merge left, right, up and down. Will create functions to reverse and transpose the list of lists to do this.
+# 3. Set up the start of the game, creating an empty gameboard filled with two random values.
+# 4. Set up the rounds of the game, where the user will have the option to merge in any one of the four directions, and after they move then new board will display.
+# 5. Set up adding a new value each time.
+# 6. Set up functions testing if the user has won or lost.
